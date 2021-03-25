c27867edbf451168f36b7994fb96517d9943f22c
Pratik Shahi <shahipr0@gmail.com>
2021-03-25 13:08:50 +0530

    finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import pie, { doublePi, triplePi } from "./math";
+import pie, { doublePi, triplePi } from "./math";
 //for default import name doesnt matter ,but for other import name shout be same
 
-import * as pie from "./math";
+//import * as pie from "./math";
 // * is doscouraged cause it imports all even if we need particula
 ReactDOM.render(
   <ul>
-    <li>{pie.default}</li>
-    <li>{pie.doublePi()}</li>
-    <li>{pie.triplePi()}</li>
+    <li>{pie}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
